a486d5c6f1b699f6867f419c20dd3b4ab7d9f8cd
Ayush <dev.ayush.ojha@gmail.com>
2025-02-25 06:43:09 +0530

    removed formatting changes

--- src/adapters/base.ts
+++ src/adapters/base.ts
@@ -2,17 +2,17 @@ export type CacheKey = string
 export type TTL = number
 
 interface CacheProviderCore<T> {
-  get(key: CacheKey): Promise<T | null>;
+    get(key: CacheKey): Promise<T | null>;
 
-  mget(...key: CacheKey[]): Promise<(T | null)[]>
+    mget(...key: CacheKey[]): Promise<(T | null)[]>
 
-  mset(...key: [CacheKey, T][]): Promise<"OK" | null>
+    mset(...key: [CacheKey, T][]): Promise<"OK" | null>
 
-  set(key: CacheKey, data: T, ttl?: TTL): Promise<"OK" | null>;
+    set(key: CacheKey, data: T, ttl?: TTL): Promise<"OK" | null>;
 
-  del(...key: CacheKey[]): Promise<number>;
+    del(...key: CacheKey[]): Promise<number>;
 
-  expire(key: CacheKey, new_ttl_from_now: TTL): Promise<0 | 1>;
+    expire(key: CacheKey, new_ttl_from_now: TTL): Promise<0 | 1>;
 }
 
 export type TReplacerFunction = (key: string, value: any) => string;
@@ -30,14 +30,14 @@ export interface CacheProvider<T> extends CacheProviderCore<T> {
 }
 
 export interface Pipeline<T> {
-  get(key: CacheKey): Pipeline<T>;
+    get(key: CacheKey): Pipeline<T>;
 
-  set(key: CacheKey, data: T, ttl?: number): Pipeline<T>;
+    set(key: CacheKey, data: T, ttl?: number): Pipeline<T>;
 
-  del(key: CacheKey): Pipeline<T>;
+    del(key: CacheKey): Pipeline<T>;
 
-  expire(key: CacheKey, new_ttl_from_now: number): Pipeline<T>;
+    expire(key: CacheKey, new_ttl_from_now: number): Pipeline<T>;
 
-  exec(): Promise<any[]>;
+    exec(): Promise<any[]>;
 }
 
